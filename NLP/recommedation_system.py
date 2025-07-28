e1c81729d7da0b223b23afe97dac022c37f61ac8
Ysk-dev <yusufsertkayaysk@gmail.com>
2025-07-28 09:22:09 +0300

    feat: add KNN-based recommendation

--- NLP/recommedation_system.py
+++ NLP/recommedation_system.py
@@ -75,5 +75,60 @@ example_user_id = 1
 example_item_id = 2
 predicted_rating = model.predict([np.array([example_user_id]), np.array([example_item_id])])
 print(f'Predicted rating for user {example_user_id} on item {example_item_id}: {predicted_rating[0][0]}')
+#%%
+! pip install numpy==1.24.4 scikit-surprise==1.1.3
+# %%
+import pandas as pd
+import numpy
+from sklearn.metrics.pairwise import cosine_similarity
+from surprise import Dataset, KNNBasic , accuracy, Reader
+from sklearn.model_selection import train_test_split
+
+# Load the MovieLens dataset
+data = Dataset.load_builtin('ml-100k')
+df = pd.DataFrame(data.raw_ratings, columns=['user_id', 'item_id', 'rating', 'timestamp'])
+df['user_id'] = df['user_id'].astype(int)
+df['item_id'] = df['item_id'].astype(int)   
+
+# train-test split
+train_data, test_data = train_test_split(df, test_size=0.2, random_state=42)
+
+model_options = {
+    'name': 'cosine',
+    'user_based': True  # True for user-based, False for item-based
+}
+
+model = KNNBasic(sim_options=model_options)
+model.fit(train_data)
+
+predictions = model.test(test_data)
+accuracy.rmse(predictions)
+
+
+# %%
+def get_top_n_recommendations(user_id, n=5):
+    user_items = df[df['user_id'] == user_id]['item_id'].tolist()
+    all_items = df['item_id'].unique().tolist()
+    items_to_predict = [item for item in all_items if item not in user_items]
+
+    if not items_to_predict:
+        return []
+
+    predictions = []
+    for item in items_to_predict:
+        pred_rating = model.predict(user_id, item).est
+        predictions.append((item, pred_rating))
+
+    predictions.sort(key=lambda x: x[1], reverse=True)
+    return predictions[:n]
+
+# Example usage
+user_id = 1
+top_n_recommendations = get_top_n_recommendations(user_id, n=5)
+print(f"Top {len(top_n_recommendations)} recommendations for user {user_id}:")
+for item, rating in top_n_recommendations:
+    print(f"Item ID: {item}, Predicted Rating: {rating:.2f}")   
+
+
 
 # %%
